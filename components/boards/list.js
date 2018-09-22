1fb00fa2ddd2632651fe2ea2000ee7a0aa8fbfe7
Hank Stoever <hstove@gmail.com>
2018-09-22 09:26:20 -0700

    fetch all boards on list page

--- components/boards/list.js
+++ components/boards/list.js
@@ -2,19 +2,46 @@ import React from 'react';
 import { Text } from 'rebass';
 import { Flex, Box } from 'grid-styled';
 import Link from 'next/link';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { CardLink } from '../card';
-import { signMessage } from '../../radiks/helpers';
+import Board from '../../models/board';
 
 class BoardsList extends React.Component {
-  componentDidMount() {
-    const signed = signMessage('radiks');
-    console.log(signed);
+  static propTypes = {
+    username: PropTypes.string.isRequired,
+  }
+
+  state = {
+    boards: [],
+  }
+
+  async componentDidMount() {
+    const boards = await Board.fetchList({
+      createdBy: this.props.username,
+    });
+    this.setState({ boards });
+  }
+
+  boards() {
+    return this.state.boards.map(board => (
+      <Box width={1 / 3} mt={4} key={board.id} mx={3}>
+        <Link href={`/boards/${board.id}`} passHref>
+          <CardLink>
+            <Text textAlign="center">
+              {board.attrs.name}
+            </Text>
+          </CardLink>
+        </Link>
+      </Box>
+    ));
   }
 
   render() {
     return (
       <Flex flexDirection="row">
+        {this.boards()}
         <Box width={1 / 3} mt={4}>
           <Link href="/boards/new" passHref>
             <CardLink>
@@ -29,4 +56,8 @@ class BoardsList extends React.Component {
   }
 }
 
-export default BoardsList;
+const mapStateToProps = state => ({
+  username: state.user.currentUser.username,
+});
+
+export default connect(mapStateToProps)(BoardsList);
