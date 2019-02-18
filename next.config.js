332e51c77b40f55dd72adde272e75007ed33328f
Hank Stoever <hstove@gmail.com>
2019-02-18 11:01:26 -0800

    babel-env

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
       apiServer: process.env.RADIKS_API_URL || 'http://localhost:3000',
     },
   },
+  env: {
+    RADIKS_API_URL: process.env.RADIKS_API_URL || 'http://localhost:3000',
+  },
 };
